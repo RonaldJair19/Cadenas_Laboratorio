114536dd8b0c4b3691ddc9d315b3d109b520ccdc
Ronald Juarez <jairudas1998@gmail.com>
2020-08-31 00:17:05 -0500

    Funcional, retocar la repeticion de cadenas

--- src/laboratorio_n1/Interfaz.java
+++ src/laboratorio_n1/Interfaz.java
@@ -4,10 +4,21 @@ package laboratorio_n1;
 //@author Ronald Juarez - Eugenio Perez
  
 public class Interfaz extends javax.swing.JFrame {
-    String Cadena_1 = "", Cadena_2 = "";
+
     String Alfabeto = "xyztXYZT";
+    
     public Interfaz() {
         initComponents();
+        
+        jTextField_long_cad1.setEnabled(false);
+        jTextField_long_cad2.setEnabled(false);
+        jButton_calcular.setEnabled(false);
+        jTextField_Concat.setEnabled(false);
+        
+        
+        
+        
+        
     }
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -25,19 +36,20 @@ public class Interfaz extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jCheckBox1 = new javax.swing.JCheckBox();
         jLabel7 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jTextField3 = new javax.swing.JTextField();
+        jButton_calcular = new javax.swing.JButton();
+        jTextField_long_cad1 = new javax.swing.JTextField();
         jLabel8 = new javax.swing.JLabel();
         jLabel9 = new javax.swing.JLabel();
         jTextField4 = new javax.swing.JTextField();
         jButton2 = new javax.swing.JButton();
         jLabel_cad2 = new javax.swing.JLabel();
         jLabel11 = new javax.swing.JLabel();
-        jTextField5 = new javax.swing.JTextField();
+        jTextField_long_cad2 = new javax.swing.JTextField();
         jLabel12 = new javax.swing.JLabel();
-        jTextField6 = new javax.swing.JTextField();
+        jTextField_Concat = new javax.swing.JTextField();
         jButton3 = new javax.swing.JButton();
         jLabel13 = new javax.swing.JLabel();
+        jLabel_Exp = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -94,7 +106,12 @@ public class Interfaz extends javax.swing.JFrame {
         jLabel7.setForeground(new java.awt.Color(0, 0, 0));
         jLabel7.setText("Longitud de las cadenas");
 
-        jButton1.setText("Calcular");
+        jButton_calcular.setText("Calcular");
+        jButton_calcular.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton_calcularActionPerformed(evt);
+            }
+        });
 
         jLabel8.setText("Cadena 1");
 
@@ -117,6 +134,11 @@ public class Interfaz extends javax.swing.JFrame {
         jLabel12.setText("Ingrese un exponente:");
 
         jButton3.setText("Calcular potencia de la cadena");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jLabel13.setBackground(new java.awt.Color(0, 0, 0));
         jLabel13.setFont(new java.awt.Font("Gill Sans Ultra Bold", 0, 18)); // NOI18N
@@ -141,15 +163,15 @@ public class Interfaz extends javax.swing.JFrame {
                             .addComponent(jLabel7))
                         .addGap(18, 18, 18)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jTextField5)
-                            .addComponent(jTextField3))
+                            .addComponent(jTextField_long_cad2)
+                            .addComponent(jTextField_long_cad1))
                         .addGap(228, 228, 228))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(jLabel11)
                                 .addGap(30, 30, 30)
-                                .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(jTextField_Concat, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(jLabel3)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
@@ -165,30 +187,31 @@ public class Interfaz extends javax.swing.JFrame {
                                             .addGroup(jPanel1Layout.createSequentialGroup()
                                                 .addGap(6, 6, 6)
                                                 .addComponent(jLabel_cad1)))
-                                        .addGap(18, 18, 18)
                                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                             .addGroup(jPanel1Layout.createSequentialGroup()
-                                                .addComponent(cadena2, javax.swing.GroupLayout.PREFERRED_SIZE, 184, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                                .addComponent(jButton2))
-                                            .addGroup(jPanel1Layout.createSequentialGroup()
                                                 .addGap(6, 6, 6)
+                                                .addComponent(jLabel_cad2))
+                                            .addGroup(jPanel1Layout.createSequentialGroup()
                                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                                    .addComponent(jButton1)
-                                                    .addComponent(jLabel_cad2)))))))
+                                                    .addComponent(jButton_calcular)
+                                                    .addComponent(cadena2, javax.swing.GroupLayout.PREFERRED_SIZE, 184, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                                .addComponent(jButton2))))))
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(jLabel2)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(jLabel13, javax.swing.GroupLayout.PREFERRED_SIZE, 274, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(jCheckBox1))
-                        .addContainerGap(12, Short.MAX_VALUE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel12)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jTextField4)
-                        .addGap(18, 18, 18)
-                        .addComponent(jButton3)
-                        .addGap(38, 38, 38))))
+                            .addComponent(jCheckBox1)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jButton3)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addComponent(jLabel12)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jLabel_Exp, javax.swing.GroupLayout.PREFERRED_SIZE, 386, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addContainerGap(24, Short.MAX_VALUE))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -221,27 +244,33 @@ public class Interfaz extends javax.swing.JFrame {
                                 .addGap(23, 23, 23))
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jTextField_long_cad1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addComponent(jLabel8))
                                 .addGap(18, 18, 18)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                     .addComponent(jLabel9)
-                                    .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                                    .addComponent(jTextField_long_cad2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(57, 57, 57)
-                        .addComponent(jButton1)))
-                .addGap(32, 32, 32)
+                        .addComponent(jButton_calcular)))
+                .addGap(33, 33, 33)
                 .addComponent(jCheckBox1)
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel11)
-                    .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(36, 36, 36)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel12)
-                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton3))
-                .addContainerGap(16, Short.MAX_VALUE))
+                    .addComponent(jTextField_Concat, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel12)
+                            .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButton3))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(29, 29, 29)
+                        .addComponent(jLabel_Exp, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(42, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -263,15 +292,29 @@ public class Interfaz extends javax.swing.JFrame {
     }//GEN-LAST:event_cadena1ActionPerformed
 
     private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
-        // TODO add your handling code here:
+        String Cadena_1 = "", Cadena_2 = "";
+        Cadena_1 = cadena1.getText();
+        Cadena_2 = cadena2.getText();
+        if (jCheckBox1.isSelected()){
+            String Cadena_concat;
+            Cadena_concat = Cadena_1.concat(Cadena_2);
+            jTextField_Concat.setEnabled(true);
+            jTextField_Concat.setText(Cadena_concat);
+        }
+        else{
+           jTextField_Concat.setEnabled(false); 
+        }
     }//GEN-LAST:event_jCheckBox1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        String Cadena_1 = "", Cadena_2 = "";
         Cadena_1 = cadena1.getText();
-        //Cadena_2 = cadena2.getText();
+        Cadena_2 = cadena2.getText();
         char aux2;
         char aux;
         char band1 = ' ';
+        char band2 = ' ';
+        //Cadena 1
         for(int i=0;i<Cadena_1.length();i++)
         {
             band1 = 'F';
@@ -291,7 +334,55 @@ public class Interfaz extends javax.swing.JFrame {
         if (band1 == 'V'){
             jLabel_cad1.setText("La cadena es valida");
         }
+        //Cadena 2
+        for(int i=0;i<Cadena_2.length();i++)
+        {
+            band2 = 'F';
+            aux2 = Cadena_2.charAt(i);
+            for(int j=0; j < Alfabeto.length();j+=1){
+                if (aux2 == Alfabeto.charAt(j)){
+                    j = Alfabeto.length();
+                    band2 = 'V';
+                }
+                if(j == Alfabeto.length()-1 && band2 == 'F'){
+                    jLabel_cad2.setText("La cadena no es valida");
+                    i = Cadena_2.length();
+                    
+                }
+            }
+        }
+        if (band2 == 'V'){
+            jLabel_cad2.setText("La cadena es valida");
+        }
+        
+        if(band1 == 'V' && band2 == 'V'){
+            jTextField_long_cad1.setEnabled(true);
+            jTextField_long_cad2.setEnabled(true);
+            jButton_calcular.setEnabled(true);
+        }
+        else{
+            jTextField_long_cad1.setEnabled(false);
+            jTextField_long_cad2.setEnabled(false);
+            jButton_calcular.setEnabled(false);  
+        }
+        
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton_calcularActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_calcularActionPerformed
+        String Cadena_1 = "", Cadena_2 = "";
+        Cadena_1 = cadena1.getText();
+        Cadena_2 = cadena2.getText();
+        jTextField_long_cad1.setText(Integer.toString(Cadena_1.length()));
+        jTextField_long_cad2.setText(Integer.toString(Cadena_2.length()));
+    }//GEN-LAST:event_jButton_calcularActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        int exponente;
+        String cadena_concat;
+        exponente = Integer.parseInt(jTextField4.getText());
+        cadena_concat = jTextField_Concat.getText();
+        jLabel_Exp.setText(cadena_concat.repeat(exponente));
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     
     
@@ -335,9 +426,9 @@ public class Interfaz extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField cadena1;
     private javax.swing.JTextField cadena2;
-    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton_calcular;
     private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel11;
@@ -350,13 +441,14 @@ public class Interfaz extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
+    private javax.swing.JLabel jLabel_Exp;
     private javax.swing.JLabel jLabel_cad1;
     private javax.swing.JLabel jLabel_cad2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JTextField jTextField3;
     private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField5;
-    private javax.swing.JTextField jTextField6;
+    private javax.swing.JTextField jTextField_Concat;
+    private javax.swing.JTextField jTextField_long_cad1;
+    private javax.swing.JTextField jTextField_long_cad2;
     // End of variables declaration//GEN-END:variables
 }
